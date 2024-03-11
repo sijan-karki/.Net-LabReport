a9f0aa9ae6a122c81b68e41f449d8d9f5996becf
SijanUnleashed <112467487+sijan-karki@users.noreply.github.com>
2024-03-11 09:08:19 +0545

    Add files via upload

---
+++ 16.(a).BuildInAttributes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabReport_Csit_SijanKarki
+{
+    public class Calculator
+    {
+        [Obsolete("Use Addnumber(List<int> numbers) method")]
+        public static int Addnumber(int a,int b)
+        {
+            return a + b;
+        }
+        public static int Addnumber(List<int> numbers)
+        {
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+    }
+    internal class BuildInAttribute
+    {
+        static void Main(string[] args)
+        {
+            int c= Calculator.Addnumber(11, 22);
+            Console.WriteLine("The sum of two number is :"+c);
+            int b = Calculator.Addnumber(new List<int> { 11, 23, 45, 67 });
+            Console.WriteLine("The sum of numbers of list : "+b);
+            Console.WriteLine("\n Lab NO :16'a'");
+            Console.WriteLine(" Name : Sijan Karki");
+            Console.WriteLine(" Roll No:28");
+        }
+       
+    }
+}
---
+++ 17.Asyncronous-Programming.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabReport_Csit_SijanKarki
+{
+    internal class AsyncronousProgrammingDemo
+    {
+        public static async void Task1()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Task 1 Starting...");
+                Thread.Sleep(5000);
+                Console.WriteLine("Task 1 Completed.");
+            });
+            Console.WriteLine("\n Lab NO :17");
+            Console.WriteLine(" Name : Sijan Karki");
+            Console.WriteLine(" Roll No:28");
+
+        }
+        public static async void Task2()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Task 2 Starting...");
+                Thread.Sleep(4000);
+                Console.WriteLine("Task 2 Completed.");
+            });
+        }
+        public static async void Task3()
+        {
+           await Task.Run(() =>
+            {
+                Console.WriteLine("Task 3 Starting...");
+                Thread.Sleep(1000);
+                Console.WriteLine("Task 3 Completed.");
+            });
+        }
+        public static async void Task4()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Task 4 Starting...");
+                Thread.Sleep(3000);
+                Console.WriteLine("Task 4 Completed.");
+            });
+        }
+        static void  Main(string[] args)
+        {
+            Task1(); 
+            Task2();
+            Task4();
+            Task3();
+            Console.ReadLine(); // to hold console window
+        }
+    }
+}
