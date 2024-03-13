11be793b75a0f0546f1bc40f97165e5e0aa88ba6
SijanUnleashed <112467487+sijan-karki@users.noreply.github.com>
2024-03-13 21:39:48 +0545

    Add files via upload

---
+++ 10(a).Non-Generic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace LabReport_Csit_SijanKarki
+{
+    internal class NonGenericCOllection
+    {
+        static void Main(string[] args)
+        {
+            ArrayList al= new ArrayList();
+            al.Add(1);
+            al.Add(2.3);
+            al.Add("sijan");
+            al.Add('A');
+            al.Add("Ram");
+            Console.WriteLine("Capacity = {0}",al.Capacity);
+            Console.WriteLine("Count = {0}",al.Count);
+            Console.WriteLine("The elements in non generic collection are ::");
+            foreach(object i in al) Console.Write(i+" ");
+            Console.WriteLine("\n Lab NO :10'a'");
+            Console.WriteLine(" Name : Sijan Karki");
+            Console.WriteLine(" Roll No:28");
+        }
+    }
+}
---
+++ 10.(b)GenericDemo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LabReport_Csit_SijanKarki
+{
+    class GenericCollectionDemo
+    {
+        static void Main(string[] args)
+        {
+            // Creating a generic List to store strings
+            List<string> stringList = new List<string>();
+
+            // Adding elements to the list
+            stringList.Add("Apple");
+            stringList.Add("Banana");
+            stringList.Add("Orange");
+
+            // Accessing elements in the list
+            Console.WriteLine("Fruits in the List:");
+            foreach (string fruit in stringList)
+            {
+                Console.WriteLine(fruit);
+            }
+
+            // Modifying an element in the list
+            stringList[1] = "Grapes";
+
+            // Removing an element from the list
+            stringList.Remove("Orange");
+
+            // Displaying the modified list
+            Console.WriteLine("\nModified List:");
+            foreach (string fruit in stringList)
+            {
+                Console.WriteLine(fruit);
+            }
+            Console.WriteLine("\n Lab NO: 10'a'");
+            Console.WriteLine(" Name: Sijan Karki");
+            Console.WriteLine(" Roll No: 28");
+        }
+    }
+}
